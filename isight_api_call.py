017bf35b88337defb981f7dc35488ec07ba104a6
Dave Dyer <houseofdyer@gmail.com>
2014-03-18 15:15:25 -0600

    Update added each type of query

--- isight_api_call.py
+++ isight_api_call.py
@@ -5,8 +5,6 @@ import hmac
 import urllib
 import urlparse
 
-PUB = mine
-PRV = mine
 
 def getData(url, path, query, pub, prv):
     hashed = hmac.new(prv, '', hashlib.sha256)
@@ -25,17 +23,41 @@ def printBasicSearch(data):
     print 'WebLink: {}'.format(data[u'webLink'])
     print 'PublishedOn: {}'.format(data[u'publishDate'])
 
-
 def parseJsonAndPrint(url, path, query, pub, prv):
     data = getData(url, path, query, pub, prv);
     content = data[u'message']
-    for report in content:
-     print ""
-     print ""
-     printBasicSearch(report)
+    return content
+    #for report in content:
+    #    print ""
+    #    print ""
+    #    printBasicSearch(report)
 
-myquery = {'md5': '098f6bcd4621d373cade4e832627b4f6', 'format': 'json'}
+#myquery = {'md5': '098f6bcd4621d373cade4e832627b4f6', 'format': 'json'}
+#ioc_query = {'format': 'json'}
+#basic_query = {}
+#adv_query = {}
 
-parseJsonAndPrint("api.isightpartners.com", "/search/basic", urllib.urlencode(myquery), PUB, PRV)
-# {u'message': {u'url': u'', u'description': u'The credentials you provided are invalid', u'error': u'unauthorized'}, u'success': False}
 
+#report_index_query = {'format': 'json'} # downloads using default time
+#report_get_query = {'format': 'json'}
+#basic_query = {'format':'json', 'Protocol':'http'}
+basic_query = {'format':'json', 'ips':'93.170.130.109'}
+#basic_query = {'format':'json', 'description':'93.170.130.109'}
+advanced_query = {'format':'json','networkIdentifier': 'Attacker'}
+warn_query = {'format':'json'}
+ioc_query = {'format':'json'}
+rep_ix_query = {'format':'json'}
+rep_query = {'format':'json'}
+
+intels_basic = parseJsonAndPrint("api.isightpartners.com", "/search/basic", urllib.urlencode(basic_query), PUB, PRV)
+intels_adv = parseJsonAndPrint("api.isightpartners.com", "/search/advanced", urllib.urlencode(advanced_query), PUB, PRV)
+intels_warn = parseJsonAndPrint("api.isightpartners.com", "/view/indicators", urllib.urlencode(warn_query), PUB, PRV)
+intels_ioc = parseJsonAndPrint("api.isightpartners.com", "/view/iocs", urllib.urlencode(ioc_query), PUB, PRV)
+intels_ix = parseJsonAndPrint("api.isightpartners.com", "/report/index", urllib.urlencode(rep_ix_query), PUB, PRV)
+intels_rep = parseJsonAndPrint("api.isightpartners.com", "/report/14-30106", urllib.urlencode(rep_query), PUB, PRV)
+
+ #{u'message': {u'url': u'', u'description': u'The credentials you provided are invalid', u'error': u'unauthorized'}, u'success': False}
+
+types_basic_search = [x[u'productType'] for x in intels_basic]
+types_warn = [x[u'productType'] for x in intels_warn]
+types_ioc = [x[u'productType'] for x in intels_ioc]
